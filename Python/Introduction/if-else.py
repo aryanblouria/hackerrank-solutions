36111719df49536350c922e62ba41cdd9c905244
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2021-11-01 20:34:35 +0530

    Create if-else.py

---
+++ Python/Introduction/if-else.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 0:
+        if n <= 5 and n >= 2:
+            print("Not Weird")
+        elif n <= 20 and n >= 6:
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:
+        print("Weird")
