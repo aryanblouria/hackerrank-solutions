a9d05d747357e125b043045359d9489b0d1b279d
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-17 14:21:28 +0530

    Create pointers.cpp

---
+++ C++/Introduction/pointers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void update(int *a,int *b) 
+{
+    int c = *a;
+     *a = *a + *b; 
+     *b = abs(c-*b);    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    cin>>a>>b;
+    update(pa, pb);
+    cout<<a<<endl<<b;
+
+    return 0;
+}
