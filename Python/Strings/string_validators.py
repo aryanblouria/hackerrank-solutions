accf50142efccf29ddd1d7e1b1d4e76dc617c627
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-22 16:19:54 +0530

    Create string_validators.py

---
+++ Python/Strings/string_validators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    s = input()
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isnumeric() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
+    
+    
