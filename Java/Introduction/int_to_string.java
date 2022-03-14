4aaf38e29c9b82b1888cef2382b2f193da5c0f75
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-03-14 22:23:15 +0530

    Create int_to_string.java

---
+++ Java/Introduction/int_to_string.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = Integer.toString(n);
+        
+        System.out.println("Good job");
+    }
+}
